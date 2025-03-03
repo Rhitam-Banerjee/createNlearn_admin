3635d55dff313ee78b45b985ab1b41cb7ac5b949
Rhitam-Banerjee <banerjeerhitam1@gmail.com>
2025-03-03 12:03:39 +0530

    UPDATE:Connected to server

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
-// const host = "https://server.brightr.club";
-const host = "http://127.0.0.1:5000";
+const host = "https://server.brightr.club";
+// const host = "http://127.0.0.1:5000";
 
 const subDomain = `${host}/api_demo`;
 
